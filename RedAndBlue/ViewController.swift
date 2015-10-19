b6b7542e9697532f6c826191296e4ad64d366e0c
Андрей Букша <andreybuksha@gmail.com>
2015-10-19 20:08:34 +0300

    Initial Commit

---
+++ RedAndBlue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RedAndBlue
+//
+//  Created by Андрей Букша on 19.10.15.
+//  Copyright © 2015 Андрей Букша. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
