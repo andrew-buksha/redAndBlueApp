5b4b421b611a7af004942ad4725e8fbd590fa079
Андрей Букша <andreybuksha@icloud.com>
2015-10-20 19:07:53 +0300

    Added my exercise RedAndBlue project

--- RedAndBlue/ViewController.swift
+++ RedAndBlue/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueGrenade: UIImageView!
+    @IBOutlet weak var redGrenade: UIImageView!
+    @IBOutlet weak var hideBlueButton: UIButton!
+    @IBOutlet weak var hideRedButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideBlue(sender: AnyObject) {
+        blueGrenade.hidden = true
+        hideBlueButton.hidden = true
+    }
 
+    @IBAction func hideRed(sender: AnyObject) {
+        redGrenade.hidden = true
+        hideRedButton.hidden = true
+    }
 }
 
